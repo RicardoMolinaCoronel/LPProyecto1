19c9c871f11b8ea6a6c69587d1faa76c05d77a5d
Cliente CONTROL-PC <CltControl@GGIECSOFWRK140.espol.edu.ec>
2023-06-12 12:00:08 -0500

    Tokens Translated

--- main.py
+++ main.py
@@ -28,7 +28,12 @@ reserved = {
             'String' : 'STRING',
             'double' : 'DOUBLE',
             'Iterable' : 'ITERABLE',
-            'Set' : 'SET'
+            'Set' : 'SET',
+            'in' : 'IN',
+            'is' : 'IS',
+            'true' : 'TRUE',
+            'false' : 'FALSE',
+            'double': 'DOUBLE'
 
 
 
@@ -36,15 +41,15 @@ reserved = {
 
 
 #Sequencia de tokens
-tokens = ('NUMERO','MAS', 'MENOS', 'POR', 'ENTRE', 'LPAREN', 'RPAREN',
-          'CAMPO','COMMENT','PUNTO','COMA','MAYORQUE','MENORQUE','DIFERENTE',
-          'COMADOBLE','CADENA','IGUAL','IGUALDOBLE',"LLLAVE","RLLAVE",
-          'LCORCH','RCORCH','PUNTOCOMA','AMPERSAND', 'COLON', 'EXMARK', 'AND', 'OR', 'APOSTROFE','METODO', 'DOLAR') + tuple(reserved.values())
+tokens = ('DIGIT','PLUS', 'MINUS', 'MULTIPASTERISK', 'DIVISION', 'LPAREN', 'RPAREN',
+          'CAMPO','COMMENT','DOT','COMA','GREATERTHAN','LESSTHAN','NOTEQUAL',
+          'DOUBQUOTMARK','STR','EQUAL','DOUBLEQUAL',"LCURLYBRACKET","RCURLYBRACKET",
+          'LSQUAREBRACKET','RSQUAREBRACKET','SEMICOLON','AMPERSAND', 'COLON', 'EXMARK', 'AND', 'OR', 'APOSTROPHE','METHOD', 'DOLLAR') + tuple(reserved.values())
 
 '''
 Contribucion Ricardo: tokens(COMMENT hasta AMPERSAND), reservadas(while hasta return) - algoritmo1
-Contribucion Jared: tokens(COLON - APOSTROFE), reservada(exit - openWrite) - algoritmo2
-Contribucion Freddy: tokens (DOLAR), reservada (Iterable - Set) - algoritmo3
+Contribucion Jared: tokens(COLON - APOSTROPHE), reservada(exit - await) - algoritmo2
+Contribucion Freddy: tokens (DOLLAR), reservada (Iterable - Set) - algoritmo3
 
 '''
 
@@ -52,35 +57,35 @@ Contribucion Freddy: tokens (DOLAR), reservada (Iterable - Set) - algoritmo3
 #Exp Regulares para tokens de símbolos
 
 
-t_NUMERO = r'\d+'
-t_MAS = r'\+'
-t_MENOS = r'-'
-t_POR = r'\*'
-t_ENTRE = r'/'
+t_DIGIT = r'\d+'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_MULTIPASTERISK = r'\*'
+t_DIVISION = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
-t_PUNTO = r'\.'
+t_DOT = r'\.'
 t_COMA = r','
-t_MAYORQUE = r'>'
-t_MENORQUE = r'<'
-t_DIFERENTE = r'<>'
-t_COMADOBLE = r'\"'
-t_CADENA = r'("[^".]*"|\'[^\'.]*\'|"[^"]*"|\'[^\']*\')'
-t_IGUAL = r'='
-t_IGUALDOBLE = r'=='
-t_LLLAVE = r'\{'
-t_RLLAVE = r'\}'
-t_LCORCH = r'\['
-t_RCORCH = r'\]'
-t_PUNTOCOMA = r';'
+t_GREATERTHAN = r'>'
+t_LESSTHAN = r'<'
+t_NOTEQUAL = r'<>'
+t_DOUBQUOTMARK = r'\"'
+t_STR = r'("[^".]*"|\'[^\'.]*\'|"[^"]*"|\'[^\']*\')'
+t_EQUAL = r'='
+t_DOUBLEQUAL = r'=='
+t_LCURLYBRACKET = r'\{'
+t_RCURLYBRACKET = r'\}'
+t_LSQUAREBRACKET = r'\['
+t_RSQUAREBRACKET = r'\]'
+t_SEMICOLON = r';'
 t_AMPERSAND = r'&'
 t_COLON = r':'
 t_EXMARK = r'!'
 t_AND = r'&&'
 t_OR = r'\|\|'
-t_APOSTROFE = r'\''
-t_METODO = r'[a-zA-Z\_]+\(\)$'
-t_DOLAR=r'\$'
+t_APOSTROPHE = r'\''
+t_METHOD = r'[a-zA-Z\_]+\(\)$'
+t_DOLLAR=r'\$'
 
 #Para contabilizar nro de líneas
 def t_newline(t):
